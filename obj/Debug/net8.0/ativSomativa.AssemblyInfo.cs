e054a690457a3c27b7c32f2561ad4c3e4fe60c28
Samuel Martins <samukamartins007@gmail.com>
2024-04-22 22:06:58 -0300

    Second commit

--- obj/Debug/net8.0/ativSomativa.AssemblyInfo.cs
+++ obj/Debug/net8.0/ativSomativa.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ativSomativa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b12cdc5b6c94ed009613ce8300634a0ecb690f66")]
 [assembly: System.Reflection.AssemblyProductAttribute("ativSomativa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ativSomativa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
